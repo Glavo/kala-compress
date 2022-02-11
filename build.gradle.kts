83f0835faab2e7fb0ebaabef56d019e57db819b3
Glavo <zjx001202@gmail.com>
2022-02-12 03:54:01 +0800

    Setup Maven Publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,23 @@
 import java.io.RandomAccessFile
+import java.util.Properties
 
 plugins {
     `java-library`
     `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
 val buildType = project.findProperty("buildType")?.toString()?.toUpperCase() ?: "SNAPSHOT"
-val baseVersion = "1.21.0.1-alpha1"
+val baseVersion = "1.21.0.1-beta1"
+
+loadMavenPublishProperties()
 
 allprojects {
     apply {
         plugin("java-library")
         plugin("maven-publish")
+        plugin("signing")
     }
 
     group = "org.glavo.kala"
@@ -69,6 +75,54 @@ allprojects {
             from(rootProject.file("LICENSE.txt"))
         }
     }
+
+    tasks.withType<GenerateModuleMetadata> {
+        enabled = false
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                version = project.version.toString()
+                artifactId = project.name
+                from(components["java"])
+
+                pom {
+                    name.set(project.name)
+                    description.set(project.description)
+                    url.set("https://github.com/Glavo/kala-compress")
+                    licenses {
+                        license {
+                            name.set("Apache 2.0")
+                            url.set("https://github.com/Glavo/kala-compress/blob/main/LICENSE.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("glavo")
+                            name.set("Glavo")
+                            email.set("zjx001202@gmail.com")
+                        }
+                    }
+                    scm {
+                        url.set("https://github.com/Glavo/kala-compress")
+                    }
+                }
+            }
+        }
+    }
+
+    if (rootProject.ext.has("signing.key")) {
+        signing {
+            useInMemoryPgpKeys(
+                rootProject.ext["signing.keyId"].toString(),
+                rootProject.ext["signing.key"].toString(),
+                rootProject.ext["signing.password"].toString(),
+            )
+            sign(publishing.publications["maven"])
+        }
+    }
 }
 
 subprojects {
@@ -106,4 +160,47 @@ tasks.test {
 tasks.processTestResources {
     from(tarTree(file("src/test/resources/zstd-tests.tar")))
     from(tarTree(resources.bzip2("src/test/resources/zip64support.tar.bz2")))
+}
+
+fun loadMavenPublishProperties() {
+    var secretPropsFile = project.rootProject.file("gradle/maven-central-publish.properties")
+    if (!secretPropsFile.exists()) {
+        secretPropsFile =
+            file(System.getProperty("user.home")).resolve(".gradle").resolve("maven-central-publish.properties")
+    }
+
+    if (secretPropsFile.exists()) {
+        // Read local.properties file first if it exists
+        val p = Properties()
+        secretPropsFile.reader().use {
+            p.load(it)
+        }
+
+        p.forEach { (name, value) ->
+            rootProject.ext[name.toString()] = value
+        }
+    }
+
+    listOf(
+        "sonatypeUsername" to "SONATYPE_USERNAME",
+        "sonatypePassword" to "SONATYPE_PASSWORD",
+        "sonatypeStagingProfileId" to "SONATYPE_STAGING_PROFILE_ID",
+        "signing.keyId" to "SIGNING_KEY_ID",
+        "signing.password" to "SIGNING_PASSWORD",
+        "signing.key" to "SIGNING_KEY"
+    ).forEach { (p, e) ->
+        if (!rootProject.ext.has(p)) {
+            rootProject.ext[p] = System.getenv(e)
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(rootProject.ext["sonatypeStagingProfileId"].toString())
+            username.set(rootProject.ext["sonatypeUsername"].toString())
+            password.set(rootProject.ext["sonatypePassword"].toString())
+        }
+    }
 }
