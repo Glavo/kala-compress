6af9ad8cbefa820849ce8a0c08fe5aace81b9351
Glavo <zjx001202@gmail.com>
2024-12-01 05:55:15 +0800

    Create module-info.java

--- archivers/sevenz/build.gradle.kts
+++ archivers/sevenz/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     implementation(libs.xz)
-    implementation(project(":kala-compress-compressors:kala-compress-compressors-bzip2"))
-    implementation(project(":kala-compress-compressors:kala-compress-compressors-deflate64"))
+    compileOnly(project(":kala-compress-compressors:kala-compress-compressors-bzip2"))
+    compileOnly(project(":kala-compress-compressors:kala-compress-compressors-deflate64"))
 }
--- archivers/zip/build.gradle.kts
+++ archivers/zip/build.gradle.kts
@@ -1,4 +1,4 @@
 dependencies {
-    implementation(project(":kala-compress-compressors:kala-compress-compressors-bzip2"))
-    implementation(project(":kala-compress-compressors:kala-compress-compressors-deflate64"))
+    compileOnly(project(":kala-compress-compressors:kala-compress-compressors-bzip2"))
+    compileOnly(project(":kala-compress-compressors:kala-compress-compressors-deflate64"))
 }
