c4272ee2521c44223caf574f15e495701fdcbc10
Glavo <zjx001202@gmail.com>
2024-12-01 22:29:39 +0800

    Release 1.27.1-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ allprojects {
     group = "org.glavo.kala"
     description = "Kala Compress"
 
-    version = "1.27.1-1" + "SNAPSHOT"
+    version = "1.27.1-1"// + "SNAPSHOT"
 
     repositories {
         mavenCentral()
